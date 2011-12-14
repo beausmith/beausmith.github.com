e1fd7b33f8bf514b5adcfe8abea897f208ed419d
Beau Smith <beau@beausmith.com>
2011-12-14 00:15:17 -0800

    add wood text experiment

---
+++ experiments/wood-type/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :nested # :expanded or :nested or :compact or :compressed
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
